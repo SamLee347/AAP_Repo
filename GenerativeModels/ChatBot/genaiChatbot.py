fb25dc1049b2ba323b21e74a014236d387ffd5b1
232878G@mymail.nyp.edu.sg <euito09@gmail.com>
2025-08-16 22:55:46 +0800

    Replace with Actual python file
    Shern Fai

--- GenerativeModels/ChatBot/genaiChatbot.py
+++ GenerativeModels/ChatBot/genaiChatbot.py
@@ -1,11 +1,38 @@
 # imports
+import base64
+import os
 from google import genai
-
+from google.genai import types
 
 # GenAI integration
 
-# This is my personal API key (ShernFai)
-client = genai.Client(api_key="AIzaSyBR-BJNTA4HSiK3b0iKmz-NZnVvSbeXCMw")
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents= "Write me a short poem about coffee.")
-print(response.text)
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+
+def generate():
+    client = genai.Client(api_key="AIzaSyBR-BJNTA4HSiK3b0iKmz-NZnVvSbeXCMw")
+    model = "gemini-2.5-flash"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""Write me a short poem about coffee."""),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        thinking_config = types.ThinkingConfig(
+            thinking_budget=-1,
+        ),
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    generate()
